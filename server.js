de5a3953450026f505ddfcb2451a24a93659709c
anu-ushka <anushkatyagi.2013@gmail.com>
2025-11-12 08:17:19 +0530

    Added auth routes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("BeeBetter backend is running successfully 🚀");
 });
